3ce6c65e38381cdaade1496496a4acedff4431cb
Deblohit <deblohitdeb01@gmail.com>
2025-11-23 21:07:48 +0530

    changed task1

--- Task1.py
+++ Task1.py
@@ -1,5 +1,5 @@
 a = int(input("Enter the first number: "))
-b = int(input("Enter the second number: "))
+b = int(input("Enter the second #: "))
 
 print("Addition: ",a+b)
 print("Subtraction: ",a-b)
